34831ba372fde200dd9334e47dd0ae350b9dca9b
HMZsuper18 <hamzah.arkoub@gmail.com>
2026-08-27 14:55:16 +0300

    chore: remove foreground service + notifications deps, use Dart engine only
    - Remove flutter_background_service, flutter_local_notifications
    - Remove foreground service permissions and manifest declaration
    - Delete background_download_service.dart
    - Simplify HybridDownloadService to delegate to ReciterStoreService
    - APK 87.3MB, AAB 73.3MB (down from 87.6/73.7)

--- lib/services/hybrid_download_service.dart
+++ lib/services/hybrid_download_service.dart
@@ -1,26 +1,13 @@
-import 'dart:async';
 import 'package:bayan/data/models/reciter_model.dart';
 import 'package:bayan/services/reciter_store_service.dart';
-import 'package:bayan/services/background_download_service.dart';
 
 class HybridDownloadService {
   HybridDownloadService._();
   static final HybridDownloadService instance = HybridDownloadService._();
 
   final ReciterStoreService _dartEngine = ReciterStoreService.instance;
-  final BackgroundDownloadService _bgService = BackgroundDownloadService.instance;
-
-  bool _bgInitialized = false;
-
-  Future<void> _ensureBgInitialized() async {
-    if (_bgInitialized) return;
-    _bgInitialized = true;
-    await _bgService.initialize();
-  }
 
   Future<void> startDownload(ReciterModel reciter) async {
-    await _ensureBgInitialized();
-    await _bgService.startDownload(reciter);
     return _dartEngine.downloadReciter(reciter);
   }
 
@@ -34,9 +21,5 @@ class HybridDownloadService {
 
   void resumeDownload(String reciterId) {
     _dartEngine.resumeDownload(reciterId);
-  }
-
-  void dispose() {
-    _bgService.dispose();
   }
 }
